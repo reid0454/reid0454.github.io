7e2b7da47fdfdcc87ffc4b6c0b83680de600446f
reid0454 <reid0454@algonquinlive.com>
2023-04-17 14:18:29 -0400

    Final Assignment
    Report to come

---
+++ lib2024/Final Assignment/Reid_Landing.js
@@ -0,0 +1,3 @@
+function myFunction() {
+  alert("Send us an email at reid0454@algonquinlive.ca");
+}
---
+++ lib2024/Final Assignment/Reid_Newsletter.js
@@ -0,0 +1,17 @@
+function myFunction() {
+  alert("Send us an email at reid0454@algonquinlive.ca");
+}
+
+function Function() {
+  if (document.getElementById("NameForm").value.length==0)
+{
+    alert("Please enter your name.")
+}
+  if (document.getElementById("EmailForm").value.length==0)
+{
+    alert("Please enter your email.")
+}
+  else {
+    alert("Thank you for subscribing!")
+  }
+}
